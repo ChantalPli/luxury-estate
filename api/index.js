b872abdd692356d8ece3aeaab521a3e0d2b76fce
Chantal <chantal.pilia01@gmail.com>
2023-10-18 10:14:36 +0200

    user update api created

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 
@@ -17,9 +18,12 @@ mongoose
 })
 
 const app = express();
+
 app.use(cors());
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.listen(3000, () =>{
   console.log('Server is running on port 3000!');
