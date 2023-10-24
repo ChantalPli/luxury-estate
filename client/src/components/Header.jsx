3981bb605b9f7a0a857a37fe3cf0ac4d44a16984
Chantal <chantal.pilia01@gmail.com>
2023-10-24 12:09:51 +0200

    header search form functionality completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,33 @@
 import { FaSearch } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Header () {
   const { currentUser } = useSelector((state) => state.user)
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate()
+
+  // we take the search term entered by the user and we add it as a parameter in the url;
+  // we then redirect the user to the corresponding search page:
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // 'URLSearchParams': method to get the info from the query
+    const urlParams = new URLSearchParams(window.location.search) // we get the searchTerm inside the url
+    urlParams.set('searchTerm', searchTerm) // the searchTerm parameter is set with the value of the current searchTerm state, which comes from the input search field
+    const searchQuery = urlParams.toString() // we convert the url parameters to a query string
+    navigate(`/search?${searchQuery}`) // we redirect the user to the new url that includes the search term
+    // to navigate the user inside this url
+  }
+
+  // we make sure that the searchTerm state variable is synchronized with the searchTerm parameter in the url:
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search) // location.search is the query string portion of the url
+    const searchTermFromUrl = urlParams.get('searchTerm') // we get the search term parameter from the query string
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl) // we update the component's local state with the retrieved search term.
+    }
+  }, [location.search])
 
   return (
     <header>
@@ -14,9 +38,17 @@ export default function Header () {
             <span>Estate</span>
           </h1>
         </Link>
-        <form className='p-3 flex items-center'>
-          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className='text-emerald-950' />
+        <form onSubmit={handleSubmit} className='p-3 flex items-center'>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-emerald-950' />
+          </button>
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
